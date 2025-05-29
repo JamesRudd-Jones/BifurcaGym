b9262982ef1caa36bff3486e07c9a6803cae5b9d
james_dell <james.rudd.jones@gmail.com>
2025-05-29 14:28:46 +0100

    adjusting discrete spaces, perhaps this will be easier

--- bifurcagym/spaces.py
+++ bifurcagym/spaces.py
@@ -11,12 +11,16 @@ class Space:
 class Discrete(Space):
     def __init__(self,
                  num_discrete: int):
-        self.num_discrete = num_discrete
-        self.shape = ()
+        self.n = num_discrete
+        self.shape = (self.n,)
+        # self.shape = ()
+        # Trialling this to make it easier from the RL side of things rather than having to do
+        # self.observation_space().shape[0] for continuous and self.observation_space().n for discrete
+        # If it causes issues then can easily change it back
         self.dtype = jnp.int_
 
     def sample(self, key: chex.PRNGKey) -> chex.Array:
-        return jrandom.randint(key, shape=self.shape, minval=0, maxval=self.num_discrete).astype(self.dtype)
+        return jrandom.randint(key, shape=(), minval=0, maxval=self.n).astype(self.dtype)
 
 
 class Box(Space):
